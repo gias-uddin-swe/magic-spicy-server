7d6590ac7b9fa40096677cda91954c47a71211de
GIAS UDDIN <gias.swe.ynu@gmail.com>
2021-04-01 13:39:36 +0800

    port updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new MongoClient(uri, {
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
